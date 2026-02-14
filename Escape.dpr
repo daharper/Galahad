b08f39ff711026a93ac3bd9a44ef9b8e78bd4350
daharper <daharper@hotmail.com>
2026-02-14 05:34:51 +0000

    Bringing in the base library

--- Escape.dpr
+++ Escape.dpr
@@ -5,7 +5,20 @@ program Escape;
 {$R *.res}
 
 uses
-  System.SysUtils;
+  System.SysUtils,
+  Base.Collections in 'Base\Base.Collections.pas',
+  Base.Container in 'Base\Base.Container.pas',
+  Base.Conversions in 'Base\Base.Conversions.pas',
+  Base.Core in 'Base\Base.Core.pas',
+  Base.Dynamic in 'Base\Base.Dynamic.pas',
+  Base.Formatting in 'Base\Base.Formatting.pas',
+  Base.Integrity in 'Base\Base.Integrity.pas',
+  Base.Json in 'Base\Base.Json.pas',
+  Base.Messaging in 'Base\Base.Messaging.pas',
+  Base.Reflection in 'Base\Base.Reflection.pas',
+  Base.Specifications in 'Base\Base.Specifications.pas',
+  Base.Stream in 'Base\Base.Stream.pas',
+  Base.Xml in 'Base\Base.Xml.pas';
 
 begin
   try
